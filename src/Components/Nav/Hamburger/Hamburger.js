5c992d0685d049e6498d2ff2245015a734522811
TomekMojsiuk <tomasz.mojsiuk@gmail.com>
2019-06-21 23:13:36 +0200

    hamburger behavior fixed

--- src/Components/Nav/Hamburger/Hamburger.js
+++ src/Components/Nav/Hamburger/Hamburger.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import './Hamburger.scss';
+import React from "react";
+import "./Hamburger.scss";
 
 class Hamburger extends React.Component {
-    constructor (props) {
-        super(props)
-        this.state = {
+ constructor(props) {
+  super(props);
+  this.state = {};
+ }
 
-        }
-    }
-
-    render () {
-        const { onClick } = this.props;
-        return (
-            <div className='hamburger' onClick={onClick}  >
-                <div className="hamburger__line"></div>
-                <div className="hamburger__line"></div>
-                <div className="hamburger__line"></div>
-            </div>
-        ) ;
-        }
-};
+ render() {
+  const { onClick, isClicked } = this.props;
+  return (
+   <div className={isClicked? "hamburger clicked": "hamburger"} onClick={onClick}>
+    <div className='hamburger__line' />
+    <div className='hamburger__line' />
+    <div className='hamburger__line' />
+   </div>
+  );
+ }
+}
 
 export default Hamburger;
