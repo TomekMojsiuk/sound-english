7bba0ccec971b07918c522449992868e5b42509e
TomekMojsiuk <tomasz.mojsiuk@gmail.com>
2019-07-19 23:02:56 +0200

    NotFound page prototype finished, mobile and dekstop version added

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -24,13 +24,21 @@ class NotFound extends React.Component {
  render() {
   const { componentDidMount } = this.state;
   return componentDidMount ? (
-   <div id="not__found">
-      <div className="bg__img"></div>
-      <div className='content__box'>
-      <h1 className='title'><span>{"4"}</span>{"0"}<span>{"4"}</span></h1>
-<h2>You're in the wrong place</h2>
-<MyLink exact to="/">Get back to reality</MyLink>
-      </div>
+   <div id='not__found'>
+    <div className='bg__img' />
+    <div className='content__box'>
+     <h1 className='title'>
+      <span>{"4"}</span>
+      {"0"}
+      <span>{"4"}</span>
+     </h1>
+     <h2>You are allowed to sream and shout now</h2>
+     <p>The page you are looking for does not exist.</p>
+     <MyLink exact to='/'>
+      Get back to reality
+     </MyLink>
+     <div className="logo"></div>
+    </div>
    </div>
   ) : (
    <Loader />
