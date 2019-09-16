b317a2713ebbdde169848d8b71f9f562ca071273
TomekMojsiuk <tomasz.mojsiuk@gmail.com>
2019-09-16 18:46:02 +0200

    lazyLoading for facebookPlugin added

--- src/Components/SocialMedia/FacebookFeed/FacebookFeed.js
+++ src/Components/SocialMedia/FacebookFeed/FacebookFeed.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./FacebookFeed.scss";
-
+import Loader from "../../Loader/Loader";
+const Facebook = React.lazy(() => import("./FacebookPlugin"));
 class FacebookFeed extends React.Component {
  constructor(props) {
   super(props);
@@ -17,16 +18,12 @@ class FacebookFeed extends React.Component {
 
  render() {
   const { isFeedVisivle } = this.state;
-
   return (
    <div id='facebook' className={isFeedVisivle ? "slide--left" : ""}>
     <div className='icon' onMouseOver={this.handleFeedVisivle} />
-    <iframe
-     src='https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FCentrum-J%C4%99zykowo-Muzyczne-Sound-English-256244201961789%2F&tabs=timeline%2C%20show_facepile&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId'
-     scrolling='no'
-     frameBorder='0'
-     allow='encrypted-media'
-    ></iframe>
+    <Suspense fallback={<Loader />}>
+     <Facebook />
+    </Suspense>
    </div>
   );
  }
--- src/Pages/OfertaCennik/OfertaCennik.js
+++ src/Pages/OfertaCennik/OfertaCennik.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "../../Pages/Pages.scss";
 import "./OfertaCennik.scss";
 
