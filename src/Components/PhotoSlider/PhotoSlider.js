7f5af9a37673adf85cfef1c2d5aa7fe9a7f6c451
TomekMojsiuk <tomasz.mojsiuk@gmail.com>
2019-09-23 00:04:53 +0200

    PhotoSlider component created. Requires logics.

--- src/Components/PhotoSlider/PhotoSlider.js
+++ src/Components/PhotoSlider/PhotoSlider.js
@@ -8,18 +8,31 @@ import img4 from '../../assets/img/PhotoGalery/SE_recording_studio.jpg';
 import img5 from '../../assets/img/PhotoGalery/SE_room_view.jpg';
 
 const PhotoSlider = (props) => {
-  const photos = document.querySelectorAll('img');
-  console.log(photos);
 
-  return (
-    <div className='photo__slider'>
-      <img src={img1} alt='microphones standing on a table' />
+  const [img, setImg] = useState({
+    img: '1'
+  });
+
+  useEffect(() => {
+    const photos = document.querySelectorAll('img');
+    console.log(photos);
+
+    photos.forEach(function(element, index) {
+      console.log(index);
+    });
+  }, []);
+
+  const images = (
+    <Fragment>
+      <img src={img1} alt='english grammar charts' />
       <img src={img2} alt='microphones standing on a table' />
-      <img src={img3} alt='microphones standing on a table' />
-      <img src={img4} alt='microphones standing on a table' />
-      <img src={img5} alt='microphones standing on a table' />
-    </div>
+      <img src={img3} alt='Sound English studio space' />
+      <img src={img4} alt='Sound English recording room' />
+      <img src={img5} alt='Sound English classroom' />
+    </Fragment>
   );
+
+  return <div className='photo__slider'>{images}</div>;
 };
 
 export default PhotoSlider;
