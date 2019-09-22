a611e1fc2b4789fa6fe18ee6b5537fb2ad4163fe
TomekMojsiuk <tomasz.mojsiuk@gmail.com>
2019-09-22 21:48:45 +0200

    Ministudio page photo changed and restyled

--- src/Pages/Ministudio/Ministudio.js
+++ src/Pages/Ministudio/Ministudio.js
@@ -1,67 +1,83 @@
-import React from "react";
-import "../Pages.scss";
-import "./Ministudio.scss";
+import React from 'react';
+import '../Pages.scss';
+import './Ministudio.scss';
 
-import Loader from "../../Components/Loader/Loader";
+import Loader from '../../Components/Loader/Loader';
 
 class Ministudio extends React.Component {
- constructor(props) {
-  super(props);
-  this.state = {
-   componentDidMount: false
-  };
- }
+  constructor(props) {
+    super(props);
+    this.state = {
+      componentDidMount: false
+    };
+  }
 
- componentDidMount() {
-  setTimeout(() => {
-   this.setState(prevState => ({
-    componentDidMount: !prevState.componentDidMount
-   }));
-  }, 1000);
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState((prevState) => ({
+        componentDidMount: !prevState.componentDidMount
+      }));
+    }, 1000);
 
-  this.props.scrollToTop();
- }
+    this.props.scrollToTop();
+  }
 
- render() {
-  const { componentDidMount } = this.state;
-  
-  return <div id="ministudio" className='content__wrapper'>
-     {componentDidMount ? "" : <Loader />}
-    <div className='flag--addon' />
-    <div className="studio__img--desktop"></div>
-    <div className='main__content'>
-     <h1 className='title'>
-      <span className="initial">M</span>inistudio
-     </h1>
-     <div className="studio__img--mobile"></div>
-     <h2>W ramach Ministudio homerecordingowego SOUND ENGLISH oferuję</h2>
+  render() {
+    const { componentDidMount } = this.state;
 
-     <ul>
-      <li>
-       <span className="initial">S</span>tudyjną rejestrację wokalistów, instrumentalistów, małych zespołów
-       wokalnych bądź wokalno – instrumentalnych
-      </li>
-      <li><span className="initial">P</span>rodukcję reklam dźwiękowych, np. na potrzeby radia</li>
-      <li><span className="initial">M</span>ix i mastering gotowych ścieżek</li>
-      <li><span className="initial">N</span>agrania lektorskie w językach angielskim i polskim</li>
-      <li>
-       <span className="initial">S</span>tworzenie i rejestrację muzyki do dowolnych celów np. relaksacyjnej bądź
-       ilustracyjnej
-       {" ("}np. do filmów albo clipów na YouTube{")"}
-      </li>
-      <li><span className="initial">S</span>
-       tworzenie melodii i aranżacji do gotowego tekstu {" ("}wraz z
-       konsultacją tekstową{")"}
-      </li>
-      <li><span className="initial">P</span>isanie tekstów piosenek</li>
-      <div className='underline' />   
-    </ul>
-    <p><span className="initial">S</span>tudio dysponuje własnym, profesjonalnym sprzętem muzycznym i studyjnym
-      oraz mnóstwem kreatywnych pomysłów właściciela</p>
-    
-    </div>
-   </div>
- }
+    return (
+      <div id='ministudio' className='content__wrapper'>
+        {componentDidMount ? '' : <Loader />}
+        <div className='flag--addon' />
+        <div className='studio__img--desktop' />
+        <div className='main__content'>
+          <h1 className='title'>
+            <span className='initial'>M</span>inistudio
+          </h1>
+          <div className='studio__img--mobile'></div>
+          <h2>W ramach Ministudio homerecordingowego SOUND ENGLISH oferuję</h2>
+
+          <ul>
+            <li>
+              <span className='initial'>S</span>tudyjną rejestrację wokalistów,
+              instrumentalistów, małych zespołów wokalnych bądź wokalno –
+              instrumentalnych
+            </li>
+            <li>
+              <span className='initial'>P</span>rodukcję reklam dźwiękowych, np.
+              na potrzeby radia
+            </li>
+            <li>
+              <span className='initial'>M</span>ix i mastering gotowych ścieżek
+            </li>
+            <li>
+              <span className='initial'>N</span>agrania lektorskie w językach
+              angielskim i polskim
+            </li>
+            <li>
+              <span className='initial'>S</span>tworzenie i rejestrację muzyki
+              do dowolnych celów np. relaksacyjnej bądź ilustracyjnej
+              {' ('}np. do filmów albo clipów na YouTube{')'}
+            </li>
+            <li>
+              <span className='initial'>S</span>
+              tworzenie melodii i aranżacji do gotowego tekstu {' ('}wraz z
+              konsultacją tekstową{')'}
+            </li>
+            <li>
+              <span className='initial'>P</span>isanie tekstów piosenek
+            </li>
+            <div className='underline' />
+          </ul>
+          <p>
+            <span className='initial'>S</span>tudio dysponuje własnym,
+            profesjonalnym sprzętem muzycznym i studyjnym oraz mnóstwem
+            kreatywnych pomysłów właściciela
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Ministudio;
