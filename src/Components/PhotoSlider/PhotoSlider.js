3dbd5bb3c34ff617f1eee7c02ebbb1a6875cc515
TomekMojsiuk <tomasz.mojsiuk@gmail.com>
2019-09-26 00:19:53 +0200

    photo galery rendering on Szkola page

--- src/Components/PhotoSlider/PhotoSlider.js
+++ src/Components/PhotoSlider/PhotoSlider.js
@@ -7,13 +7,13 @@ import img3 from '../../assets/img/PhotoGalery/SE_ministudio.jpg';
 import img4 from '../../assets/img/PhotoGalery/SE_recording_studio.jpg';
 import img5 from '../../assets/img/PhotoGalery/SE_room_view.jpg';
 
-const PhotoSlider = () => {
+const PhotoSlider = (props) => {
   const [imgIndex, setImgIndex] = useState(0);
 
-  // Grab all images
-  const photos = document.querySelectorAll('.slide__img');
-
   useEffect(() => {
+    // Grab all images
+    let photos = document.querySelectorAll('.slide__img');
+
     photos.forEach(function(element, index) {
       if (index === imgIndex) {
         element.classList.add('show__img');
@@ -21,9 +21,8 @@ const PhotoSlider = () => {
         element.classList.remove('show__img');
       }
     });
-  });
 
-  useEffect(() => {
+    // Loop through the photoSlider
     if (imgIndex === photos.length) {
       setImgIndex(0);
     }
@@ -31,6 +30,15 @@ const PhotoSlider = () => {
       setImgIndex(photos.length - 1);
     }
   });
+
+  //   useEffect(() => {
+  //     // if (imgIndex === photos.length) {
+  //     //   setImgIndex(0);
+  //     // }
+  //     // if (imgIndex === -1) {
+  //     //   setImgIndex(photos.length - 1);
+  //     // }
+  //   });
 
   // Buttons actions onClick
   const imgPrev = () => setImgIndex(imgIndex + -1);
@@ -46,6 +54,9 @@ const PhotoSlider = () => {
       e.preventDefault();
       imgNext();
     }
+    if (e.key === 'Escape') {
+      props.handleShowPhotoGallery();
+    }
   };
 
   useEffect(() => {
@@ -54,8 +65,6 @@ const PhotoSlider = () => {
       window.removeEventListener('keydown', onKeyPressEvent);
     };
   });
-
-  console.log(imgIndex);
 
   const images = (
     <Fragment>
@@ -69,12 +78,21 @@ const PhotoSlider = () => {
 
   return (
     <div className='slider__photos'>
-        <div className="slider__exit"/>
+      <div className='slider__exit' onClick={props.handleShowPhotoGallery} />
       {images}
       <div className='slider__buttons'>
         <button onClick={imgPrev}>&#10094;</button>
         <button onClick={imgNext}>&#10095;</button>
       </div>
+      <p className='credentials'>
+        Zdjęcia wnętrz Sound English wykonała{' '}
+        <a
+          href={'https://www.facebook.com/jullss.julia/?ref=profile_intro_card'}
+          target='_blank'
+          rel='noopener noreferrer'>
+          Julia Tusk
+        </a>
+      </p>
     </div>
   );
 };
